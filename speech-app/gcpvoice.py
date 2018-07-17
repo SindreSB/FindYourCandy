06f253344de11aae6b8dd1d69359dc13eba61526
Sindre Sk[land brun <sindre.brun@outlook.com>
2018-07-17 15:42:45 +0200

    Add first draft of simple speech to text server

---
+++ speech-app/gcpvoice.py
@@ -0,0 +1,59 @@
+from flask import Flask
+from flask_sockets import Sockets
+
+# GCP imports
+from google.cloud.speech_v1 import enums
+from google.cloud.speech_v1 import SpeechClient
+from google.cloud.speech_v1 import types
+
+app = Flask(__name__)
+sockets = Sockets(app)
+
+
+@sockets.route('/')
+def audio(ws):
+    while True:
+        def generator():
+            msg = ws.receive()
+            if msg is not None:
+                yield(bytes(msg))
+
+        # Get the SpeechClient and configuration
+        client = SpeechClient()
+        config = types.StreamingRecognitionConfig(
+            config=types.RecognitionConfig(
+                encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+                sample_rate_hertz=16000,
+                language_code='en-US',
+            ),
+            interim_results=True,
+        )
+
+        # Create requests using the audio generator, adapted from transcribe-streaming sample
+        requests = [types.StreamingRecognizeRequest(audio_content=content) for content in generator()]
+        responses = client.streaming_recognize(config, requests)
+
+        for response in responses:
+            # Once the transcription has settled, the first result will contain the
+            # is_final result. The other results will be for subsequent portions of
+            # the audio.
+            for result in response.results:
+                print('Finished: {}'.format(result.is_final))
+                print('Stability: {}'.format(result.stability))
+                alternatives = result.alternatives
+                # The alternatives are ordered from most likely to least.
+                for alternative in alternatives:
+                    print('Confidence: {}'.format(alternative.confidence))
+                    print(u'Transcript: {}'.format(alternative.transcript))
+                    ws.send(alternative.transcript)
+                # Return/terminate if we have received the final result
+                if result.is_final:
+                    ws.close(1000)
+                    return
+
+
+if __name__ == "__main__":
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+    server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+    server.serve_forever()
---
+++ speech-app/test/audio_sender.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+
+"""Based on Google Cloud Speech API sample application using the streaming API.
+
+Example usage:
+    python audio_sender.py resources/audio.raw
+"""
+
+# [START import_libraries]
+import argparse
+import io
+
+import asyncio
+import websockets # pip install websockets
+
+
+# [START def_transcribe_streaming]
+async def transcribe_streaming(stream_file):
+    """Streams raw data of the given audio file."""
+    # [START migration_streaming_request]
+    with io.open(stream_file, 'rb') as audio_file:
+        content = audio_file.read()
+
+    # In practice, stream should be a generator yielding chunks of audio data.
+    stream = [content]
+
+    async with websockets.connect('ws://localhost:5000') as websocket:
+        for content in stream:
+            await websocket.send(content)
+
+        async for message in websocket:
+            print(message)
+
+# [END def_transcribe_streaming]
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('stream', help='File to stream to the API')
+    args = parser.parse_args()
+
+    asyncio.get_event_loop().run_until_complete(transcribe_streaming(args.stream))
